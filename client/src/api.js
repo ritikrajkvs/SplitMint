12af78daa2db349081d445046d912b05c32ce3a0
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2026-02-04 00:04:55 +0530

    Fix comment to clarify token attachment in headers

--- client/src/api.js
+++ client/src/api.js
@@ -6,13 +6,15 @@ const api = axios.create({
   baseURL: API_URL,
 });
 
-// Automatically add the token to headers
+// FIX: Automatically attach the token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
